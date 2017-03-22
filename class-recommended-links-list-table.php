ccb5f97cfe880d2b1ea012466a4359518e75f2e4
Eric <eric@epsilon11.com>
2017-03-22 01:31:25 -0500

    Added comments

--- class-recommended-links-list-table.php
+++ class-recommended-links-list-table.php
@@ -20,6 +20,11 @@
 require_once( ABSPATH . '/wp-admin/includes/admin.php' );
 
 class RecommendedLinksListTable extends WP_List_Table {
+
+  /**
+   * Constructor.  Calls the WP_List_Table constructor with parameters useful
+   * for this class.
+   */
   public function __construct() {
     parent::__construct(array(
       'plural' => 'links',
@@ -28,18 +33,17 @@ class RecommendedLinksListTable extends WP_List_Table {
     ));
   }
 
-  public function manage_columns() {
-    return array(
-      'cb' => 'cb',
-      'name' => 'Name',
-      'url' => 'URL',
-      'description' => 'Description',
-      'display_mode' => 'Display mode',
-      'created' => 'Created'
-    );
-  }
+  /**
+   * Filter callback passed to
+   * "manage_settings_page_e11_recommended_links_columns" hook.  The
+   * "manage_{$screen->id}_columns" hook is created in get_column_headers()
+   * in wp-admin/includes/screen.php and used by
+   * WP_List_Table::get_column_info() to get the list of columns.
+   *
+   * @return array Associative array of fieldname => "Displayed label"
+   */
 
-  public function get_columns() {
+  public function manage_columns() {
     return array(
       'cb' => '<input type="checkbox" />',
       'name' => 'Name',
@@ -50,6 +54,36 @@ class RecommendedLinksListTable extends WP_List_Table {
     );
   }
 
+  /**
+   * Mandatory override from WP_List_Table.  I thought this did what
+   * manage_columns() is doing in this class.  It's called in the constructor
+   * of WP_List_Table, but doesn't seem to take effect?
+   *
+   * [TODO] Figure out how to get this to work, to get rid of manage_columns()
+   * if it's redundant.  It's definitely not getting called at present for
+   * some reason.
+   *
+   * @return array
+   */
+  public function get_columns() {
+    exit(0);
+//    return array(
+//      'cb' => '<input type="checkbox" />',
+//      'name' => 'Name',
+//      'url' => 'URL',
+//      'description' => 'Description',
+//      'display_mode' => 'Display mode',
+//      'created' => 'Created'
+//    );
+  }
+
+  /**
+   * Return list of columns that can be sorted by clicking the column header.
+   *
+   * @return array Associative array, field_name => sort_field.  The
+   *               "sort_field" can be array('sort_field', true) to indicate
+   *               it should be sorted in descending order on first click.
+   */
   protected function get_sortable_columns() {
     return array(
       'name' => 'name',
@@ -58,11 +92,24 @@ class RecommendedLinksListTable extends WP_List_Table {
     );
   }
 
+  /**
+   * Custom output for "cb" column (the checkbox at the left for row selection)
+   *
+   * @param object $item Link record
+   * @return string HTML output for field
+   */
   public function column_cb($item) {
     return '<input id="cb-select-' . $item->id
               . '" type="checkbox" name="link[]" value="' . $item->id . '" />';
   }
 
+  /**
+   * Custom output for "display_mode" column.  Convert integer to label for
+   * display.
+   *
+   * @param object $item Link record
+   * @return string HTML output for field
+   */
   public function column_display_mode($item) {
     switch($item->display_mode) {
       case 1:
@@ -78,15 +125,29 @@ class RecommendedLinksListTable extends WP_List_Table {
     return '(invalid)';
   }
 
+  /**
+   * Default handler for column display
+   *
+   * @param object $item Link record
+   * @param string $column_name Field to display
+   * @return mixed Field value
+   */
   public function column_default($item, $column_name)
   {
     return $item->$column_name;
   }
 
+  /**
+   * [TODO] Write.
+   */
   public function ajax_user_can() {
     die( 'function WP_List_Table::ajax_user_can() must be over-ridden in a sub-class.' );
   }
 
+  /**
+   * Mandatory override for reading a range of items from the database for
+   * display based on parameters (possibly page, sort field, and sort order.)
+   */
   public function prepare_items() {
     global $wpdb;
 
@@ -143,5 +204,5 @@ class RecommendedLinksListTable extends WP_List_Table {
 
 }
 
-add_filter('manage_settings_page_e11_recommended_links_columns',
-                        array('RecommendedLinksListTable', 'manage_columns'));
+//add_filter('manage_settings_page_e11_recommended_links_columns',
+//                        array('RecommendedLinksListTable', 'manage_columns'));
