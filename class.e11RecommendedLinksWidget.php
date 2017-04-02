fdf65028439eadf105e4dcb60f901e5363037a8d
Eric <eric@epsilon11.com>
2017-04-02 15:49:50 -0500

    Added config form to widget

--- class.e11RecommendedLinksWidget.php
+++ class.e11RecommendedLinksWidget.php
@@ -26,7 +26,8 @@ class e11RecommendedLinksWidget extends \WP_Widget {
       'classname' => 'e11-recommended-links-widget',
       'description' => 'Show random list of admin-recommended links',
     );
-    parent::__construct( 'e11-recommended-links-widget', 'e11 Recommended Links', $widget_ops );
+    parent::__construct('e11-recommended-links-widget',
+                            'e11 Recommended Links', $widget_ops);
   }
 
   /**
@@ -35,19 +36,24 @@ class e11RecommendedLinksWidget extends \WP_Widget {
    * @param array $args
    * @param array $instance
    */
-  public function widget( $args, $instance ) {
+  public function widget($args, $instance) {
     global $wpdb;
 
     $linksTableName = $wpdb->prefix . 'e11_recommended_links';
 
     // Display widget title
 
-    $title = apply_filters( 'widget_title', empty($instance['title']) ? __( 'Recommended links' ) : $instance['title'],
-      $instance, $this->id_base );
+    if (empty($instance['title'])) {
+        $title = __('Recommended links', 'e11-recommended-links');
+    } else {
+        $title = $instance['title'];
+    }
+
+    $title = apply_filters('widget_title', $title);
 
     echo $args['before_widget'];
 
-    if ( $title ) {
+    if (!empty($title)) {
       echo $args['before_title'] . $title . $args['after_title'];
     }
 
@@ -67,18 +73,82 @@ class e11RecommendedLinksWidget extends \WP_Widget {
     foreach ($links as $link) {
 ?>
       <div class="link">
-        <a class="link-title" href="<?php echo esc_url($link->url); ?>"><?php echo $link->title; ?></a>
-        <div class="link-description"><?php echo $link->description; ?></div>
+        <a class="link-title" href="<?php echo esc_url($link->url); ?>">
+          <?php echo $link->title; ?>
+        </a>
+        <div class="link-description">
+          <?php echo $link->description; ?>
+        </div>
       </div>
 <?php
     }
 
     echo $args['after_widget'];
   }
+
+  /**
+   * Config form, used to allow the administrator to modify settings for the
+   * widget.
+   *
+   * @param array $instance Current settings.
+   * @return string ?
+   */
+  public function form($instance) {
+    if (empty($instance['title'])) {
+      $title = __('Recommended links', 'e11-recommended-links');
+    } else {
+      $title = $instance['title'];
+    }
+
+?>
+    <p>
+      <label for="<?php echo esc_attr($this->get_field_id('title')); ?>">
+        <?php esc_attr_e('Title:', 'e11-recommended-links'); ?>
+      </label>
+      <input class="widefat"
+             id="<?php echo esc_attr($this->get_field_id('title')); ?>"
+             name="<?php echo esc_attr($this->get_field_name('title')); ?>"
+             type="text" value="<?php echo esc_attr($title); ?>" />
+    </p>
+<?php
+  }
+
+  /**
+   * If $value is set, sanitize and return it.  Otherwise, return
+   * $default_value.
+   *
+   * @param string $value Value to test/sanitize
+   * @param string $default_value Value to return if $value not set
+   * @return string Sanitized $value or $default_value
+   */
+  private function set_with_default($value, $default_value)
+  {
+    if (empty($value)) {
+      return $default_value;
+    }
+
+    return strip_tags($value);
+  }
+
+  /**
+   * Sanitize and save widget form configuration.
+   *
+   * @param array $new_instance Incoming settings to save
+   * @param array $old_instance Previous settings
+   * @return array Updated safe values to be saved
+   */
+  public function update($new_instance, $old_instance)
+  {
+    $instance = array();
+
+    $instance['title'] = $this->set_with_default($new_instance['title'], '');
+
+    return $instance;
+  }
 }
 
-add_action( 'widgets_init', function(){
-  register_widget( 'e11RecommendedLinksWidget' );
+add_action('widgets_init', function() {
+  register_widget('e11RecommendedLinksWidget');
 });
 
 ?>
